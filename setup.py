d47d0f9296846222794819760e53c676ecdc9ba6
Madhur Adlakha <madhuradlakha@yahoo.co.in>
2016-01-31 18:15:46 +0530

    Python 2.x and 3.x compatible

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="geeksdownloader",
-	version="1.0.6",
+	version="1.0.7",
 	description="Download all GeeksforGeeks articles/posts of a particular topic/company within a minute",
 	url="https://github.com/madhuradlakha/geeksdownloader",
 	author="Madhur Adlakha",
